c097398c364a82d33c394aa00413723811a2a144
Juliana Fogg <julianafogg@gmail.com>
2017-10-18 02:20:06 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
         @receiver.balance += @amount
         @status = "complete"
       else
-        @status == "rejected"
+        valid? && @status == "rejected"
         "Transaction rejected. Please check your account balance."
       end
     end
