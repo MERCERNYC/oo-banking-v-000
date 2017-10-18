efc75d03643a27b5a13952fa3a23d771e49a5529
Juliana Fogg <julianafogg@gmail.com>
2017-10-18 02:06:32 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,8 +16,8 @@ class Transfer
 
   def execute_transaction
     if self.valid? && @status == "pending"
-        sender.balance -= amount
-        receiver.balance += amount
+        @sender.balance -= amount
+        @receiver.balance += amount
         @status = "complete"
       else
         @status = "rejected"
