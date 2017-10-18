4deec8b7ebe16d7ebf0db18b25d421d67f975bf9
Juliana Fogg <julianafogg@gmail.com>
2017-10-18 02:05:59 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,4 +24,8 @@ class Transfer
         "Transaction rejected. Please check your account balance."
       end
     end
+
+    def reverse_transfer
+      self.execute_transaction.reverse
+    end 
 end
